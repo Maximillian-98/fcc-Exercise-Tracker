1b3e305664ebdcf2e7a2edba9572b6f1c494b177
Maximillian Atherton <max@a319.co.uk>
2024-11-15 16:47:42 +0000

    Basic layout started

--- index.js
+++ index.js
@@ -9,9 +9,64 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+//Array to store users
+users = []
 
+app.post('/api/users', (req, res) => {
+  usernameString = req.body.username
+  idString = Math.random.toString(36).substring(2, 22)
 
+  userObject = {
+    _id: idString,
+    username: usernameString
+  }
+  //userObject[id] = usernameString
+  //users.append(userObject)
 
+  res.json({
+    username: usernameString,
+    _id: idString
+  })
+})
+
+app.get('/api/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  idString = req.body._id
+  descriptionString = req.body.description
+
+  //return single user object based on id
+  user = users.find((user) => user._id === userId);
+
+  // duration validation (number)
+  durationNum = Number(req.body.duration)
+
+  //date validation (Date() method)
+  dateString = new Date(req.body.date)
+  dateString = dateString.toDateString()
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    date: dateString,
+    duration: durationNum,
+    description: descriptionString
+  })
+})
+
+app.get('/api/users/:id/logs', (req, res) => {
+  idString = req.params.id
+  
+  res.json({
+    id:,
+    username:,
+    count:,
+    logs:
+    //array of objects that have description duration and date
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
